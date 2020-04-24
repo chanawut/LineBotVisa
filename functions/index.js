ecead2507f35e369b050426e69dcc8f0feb6d4b0
Chawutwut Juntapon <chanawut@Chawutwuts-MacBook-Pro.local>
2020-04-24 15:17:09 +0700

    start use git

--- functions/index.js
+++ functions/index.js
@@ -32,7 +32,7 @@ const reply = (bodyResponse) => {
       messages: [
         {
           type: `text`,
-          text: bodyResponse.events[0].message.text + ' 5556666'
+          text: bodyResponse.events[0].message.text + ' 5556666 from mac'
         }
 	  ]
     })
